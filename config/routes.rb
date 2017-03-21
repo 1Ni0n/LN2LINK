175c1f51461c4699f3440dc416e9ef615ce71490
Anteo Guillot <anteo.guillot@gmail.com>
2017-03-21 11:30:23 +0100

    simplified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/workshop'
-
-  get 'pages/ALaCarte'
-
-  get 'pages/Formation'
+  root to: 'pages#home'
+  get 'home', to: 'pages#home'
+  get 'workshop', to: 'pages#workshop'
+  get 'alacarte', to: 'pages#ALaCarte'
+  get 'formation', to: 'pages#Formation'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
